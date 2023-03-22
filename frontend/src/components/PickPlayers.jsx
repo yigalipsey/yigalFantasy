755cc2adb01706bf1bb8876c43b61e465b912c40
yigal lipsey <yigalipsey1996@gmail.com>
2023-03-22 17:48:54 +0200

    ok

--- frontend/src/components/PickPlayers.jsx
+++ frontend/src/components/PickPlayers.jsx
@@ -19,6 +19,12 @@ const PickPlayers = () => {
   const [coachOfTeamError, setCoachOfTeam] = useState(false)
   const [teamElevenError, setTeamElevenError] = useState(false)
 
+  const [errors, setErrors] = useState({
+    teamNameErro: 'null',
+    coachOfTeamErro: 'null',
+    teamElevenErro: 'null',
+  })
+
   //Text of eroors
   const teamNameErrorDetails = 'בחירת שם קבוצה'
   const coachOfTeamErrorDetails = 'בחירת שם מאמן'
@@ -37,18 +43,26 @@ const PickPlayers = () => {
   //function for build user team
   const handleBuildTeam = async (e) => {
     if (teamName === null) {
-      setTeamNameError(true)
-      setHasError(true)
+      const updatedErrors = { ...errors, teamNameErro: 'error' }
+      setErrors(updatedErrors)
+
+      // setTeamNameError(true)
+      // setHasError(true)
     }
 
     if (coachOfTeam === null) {
-      setCoachOfTeam(true)
-      setHasError(true)
+      const updatedErrors = { ...errors, coachOfTeamErro: 'error' }
+      setErrors(updatedErrors)
+      // setCoachOfTeam(true)
+      // setHasError(true)
     }
 
-    if (team.length < 9) {
-      setTeamElevenError(true)
-      setHasError(true)
+    if (team.length < 3) {
+      const updatedErrors = { ...errors, teamElevenErro: 'error' }
+      setErrors(updatedErrors)
+
+      // setTeamElevenError(true)
+      // setHasError(true)
     }
 
     //only if everithing filled build the team
@@ -61,7 +75,7 @@ const PickPlayers = () => {
 
   return (
     <div className=' w-5/6 md:w-4/6 mx-auto '>
-      {teamNameError && coachOfTeamError && teamElevenError ? (
+      {/* {teamNameError && coachOfTeamError && teamElevenError ? (
         <div className='z-14 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500 text-white p-4 rounded-lg h-[100px] w-1/2 bg-black flex flex-col'>
           <div>{teamNameErrorDetails}</div>
           <div>{coachOfTeamErrorDetails}</div>
@@ -76,11 +90,28 @@ const PickPlayers = () => {
         coachOfTeamError &&
         teamElevenError && (
           <div className='z-14 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500 text-white p-4 rounded-lg h-[100px] w-1/2 bg-black flex flex-col'>
-            <div>{teamNameErrorDetails}</div>
+            <div>{teamElevenErrorDetails}</div>
             <div>{coachOfTeamErrorDetails}</div>
           </div>
         )
-      )}
+      )} */}
+
+      {
+        <div className=' z-14 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500 text-white p-4 rounded-lg h-[100px] w-1/2 bg-black flex flex-col'>
+          <div className=' text-emerald-100'>
+            {errors.teamNameErro === 'error' && <h1>team name missing</h1>}
+          </div>
+          <div className=' text-emerald-100'>
+            {errors.coachOfTeamErro === 'error' && <h1>team coach missing</h1>}
+          </div>
+          <div className=' text-emerald-100'>
+            {errors.teamElevenErro === 'error' && (
+              <h1>teamElevenErro missing</h1>
+            )}
+          </div>
+        </div>
+      }
+
       <div className='flex w-full'>
         <Inputs />
       </div>
