3d133adc94f8f59df94ebb0303ddfe92afb5b4c4
yigal lipsey <yigalipsey1996@gmail.com>
2023-03-24 15:25:01 +0300

    ok

--- frontend/src/pages/Leagues.jsx
+++ frontend/src/pages/Leagues.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
 import { useFetchData } from '../hooks/useFetchData'
+import { useLeaguesContext } from '../hooks/useLeaguesContext'
 
-function League({ teams }) {
+function League() {
   const { fetchAllUsersTeams } = useFetchData()
-
+  const { mainLeague } = useLeaguesContext()
   useEffect(() => {
     const fetchData = async () => {
       await fetchAllUsersTeams()
@@ -11,25 +12,25 @@ function League({ teams }) {
     fetchData()
   }, [])
   return (
-    <div className='bg-white rounded-lg shadow-md p-2'>
+    <div className='bg-white rounded-lg shadow-md p-[300px]'>
       <table className='table-auto w-full'>
         <thead>
           <tr>
-            <th className='px-4 py-2'>#</th>
-            <th className='px-4 py-2'>Team Name</th>
-            <th className='px-4 py-2'>Coach</th>
-            <th className='px-4 py-2'>Total Points</th>
+            <th className='px-4 py-2'>דירוג</th>
+            <th className='px-4 py-2'>שם קבוצה</th>
+            <th className='px-4 py-2'>מאמן הקבוצה</th>
+            <th className='px-4 py-2'>נקודות</th>
           </tr>
         </thead>
         <tbody>
-          {/* {teams.map((team, index) => (
+          {mainLeague.map((team, index) => (
             <tr key={team.teamName}>
               <td className='border px-4 py-2'>{index + 1}</td>
               <td className='border px-4 py-2'>{team.teamName}</td>
-              <td className='border px-4 py-2'>{team.teamCoach}</td>
+              <td className='border px-4 py-2'>{team.coachOfTeam}</td>
               <td className='border px-4 py-2'>{team.totalPoints}</td>
             </tr>
-          ))} */}
+          ))}
         </tbody>
       </table>
     </div>
