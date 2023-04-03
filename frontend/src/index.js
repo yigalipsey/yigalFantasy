db84bd4ef62d3a89f6e727d51ea945b3e93cdd7c
yigal lipsey <yigalipsey1996@gmail.com>
2023-04-03 17:57:55 +0300

    Fantasy-

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import { DataProvider } from './context/DataContext'
 import { MyTeamProvider } from './context/MyTeamContext'
 import { LeaguesProvider } from './context/LeaguesContext'
 import { BrowserRouter } from 'react-router-dom'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
