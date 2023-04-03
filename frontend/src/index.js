20f995f18d506008539702f890b7c1b70a943b53
yigal lipsey <yigalipsey1996@gmail.com>
2023-04-03 19:01:27 +0300

    ok

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
